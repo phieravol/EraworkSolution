7b26c0d58161f3469e779d7000f80e97f58e5467
phinqevol <phinqevol@gmail.com>
2023-02-04 23:04:16 +0700

    PRN221 - fix bug for migration database

---
+++ Data/EntityDbContext/EraWorkContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.EntityDbContext
+{
+    public class EraWorkContextFactory : IDesignTimeDbContextFactory<EraWorkContext>
+    {
+        public EraWorkContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("EraWorkDb");
+            var optionBuilder = new DbContextOptionsBuilder<EraWorkContext>();
+            optionBuilder.UseSqlServer(connectionString);
+
+            return new EraWorkContext(optionBuilder.Options);
+        }
+    }
+}
