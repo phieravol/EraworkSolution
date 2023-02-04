21ea4cca67264115853059465767444681a272f1
phinqevol <phinqevol@gmail.com>
2023-02-04 18:25:17 +0700

    test error

---
+++ Data/Confiuration/PakageConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Confiuration
+{
+    public class PakageConfiguration : IEntityTypeConfiguration<Pakage>
+    {
+        public readonly bool ActivePakageDefault = true;
+        public void Configure(EntityTypeBuilder<Pakage> builder)
+        {
+            // config for table name Pakage
+            builder.ToTable("Pakage");
+
+            // config for primary key PakageId
+            builder.HasKey(x => x.PakageId);
+
+            // config pakage name is nvarchar(100)
+            builder.Property(x => x.PakageName)
+                .IsUnicode(true)
+                .HasMaxLength(100);
+
+            //config price money for pakage
+            builder.Property(x => x.Price)
+                .IsRequired();
+               
+            //config for revision limit is nvarchar(500)
+            builder.Property(x => x.RevisionLimit)
+                .IsUnicode(true)
+                .HasMaxLength(250);
+            // config default value for isPakageActive
+            builder.Property(x => x.isPakageAcive)
+                .IsRequired()
+                .HasDefaultValue(ActivePakageDefault);
+
+            // config reference key for Service Id
+            builder.HasOne(s => s.Service)
+                .WithMany(p => p.Pakages)
+                .HasForeignKey(s => s.ServiceId);
+
+
+        }
+    }
+}
---
+++ Data/Confiuration/SubCategoryConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Confiuration
+{
+    public class SubCategoryConfiguration : IEntityTypeConfiguration<SubCategory>
+    {
+        private readonly bool ActiveSubCate = true;
+        public void Configure(EntityTypeBuilder<SubCategory> builder)
+        {
+            // Config table name SubCategory
+            builder.ToTable("SubCategory");
+
+            // Config primary key for SubCategoryId
+            builder.HasKey(x => x.SubCateId);
+
+            // config subcategory is nvarchar(250)
+            builder.Property(x => x.SubcateName)
+                .IsUnicode(true)
+                .HasMaxLength(250);
+
+            // config subcategory desc is nvarchar(1000)
+            builder.Property(x => x.SubcateDesc)
+                .IsUnicode(true)
+                .HasMaxLength(1000);
+
+            // Config default value for isSubCateActive
+            builder.Property(x => x.isSubCateActive)
+                .IsRequired()
+                .HasDefaultValue(ActiveSubCate);
+
+            // Config reference key for CategoryId
+            builder.HasOne(c => c.Category)
+                .WithMany(s => s.SubCategories)
+                .HasForeignKey(c => c.CategoryId);
+        }
+
+    }
+}
+
