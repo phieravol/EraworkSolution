0b96fc70c6496c3cd5036a3b512ed29f2207ca62
phinqevol <phinqevol@gmail.com>
2023-02-04 18:25:17 +0700

    test error with migration

---
+++ Data/EntityDbContext/EraWorkContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.EntityDbContext
+{
+    public class EraWorkContextFactory : IDesignTimeDbContextFactory<EraWorkContext>
+    {
+        public EraWorkContext CreateDbContext(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<EraWorkContext>();
+            var connectionString = configuration.GetConnectionString("EraWorkDb");
+            optionsBuilder.UseSqlServer(connectionString);
+            return new EraWorkContext(optionsBuilder.Options);
+        }
+    }
+}
