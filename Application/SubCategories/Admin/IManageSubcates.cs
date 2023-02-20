ea5065cb3f84644198606a4bf3985d1d30a97012
phinqevol <phinqevol@gmail.com>
2023-02-20 22:47:40 +0700

    PRN221-f16: Search Admin Subcategory & Create SubCategory

---
+++ Application/SubCategories/Admin/IManageSubcates.cs
@@ -0,0 +1,18 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModels.SubCatesViewModel;
+using ViewModels.SubCatesViewModel.Admin;
+
+namespace AppModules.SubCategories.Admin
+{
+    public interface IManageSubcates
+    {
+        Task<List<SubCateVM>> PagingSubcategoriesAsync(SubcatePagingRequest request);
+        Task CreateSubcateAsync(SubcateCreateRequest request);
+        Task<List<SubCategory>>? GetSubCatesAsync();
+    }
+}
---
+++ ViewModels/SubCatesViewModel/SubCateVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels.SubCatesViewModel
+{
+    public class SubCateVM
+    {
+        public int SubCateId { get; set; }
+        public string? SubcateName { get; set; }
+        public string? SubcateImage { get; set; }
+        public bool? isSubCateActive { get; set; }
+        public string? SubcateDesc { get; set; }
+        public int? CategoryId { get; set; }
+        public string? CategoryName { get; set;}
+    }
+}
